2ec5dccf9140da501e82d6de453409dfcfae6040
Evgeniy Lyuty <elyuty@degiro.nl>
2021-03-04 22:08:56 +0100

    Refactoring.

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-
 const list = document.getElementById('myUl');
 
+const listItems = list.getElementsByTagName('LI');
+for (let i = 0; i < listItems.length; i++) {
+    initListItem(listItems[i]);
+}
+
 function initListItem(listItem) {
-    // Create a 'close' item and append it to a list item
-    const span = document.createElement('SPAN');
-    span.className = 'close';
-    span.onclick = function () {
-        const listItem = this.parentElement;
-        listItem.parentElement.removeChild(listItem);
-        saveListToLocalStorage();
-    }
-
-    const txt = document.createTextNode('\u00D7');
-    span.appendChild(txt);
-
-    listItem.appendChild(span);
+    listItem.appendChild(createCloseButton());
 
     listItem.onclick = function () {
         this.classList.toggle('checked');
@@ -22,47 +14,59 @@ function initListItem(listItem) {
     }
 }
 
-const listItems = document.getElementsByTagName('LI');
-for (let i = 0; i < listItems.length; i++) {
-    initListItem(listItems[i]);
+function createCloseButton() {
+    const closeButton = document.createElement('SPAN');
+    closeButton.className = 'close';
+
+    closeButton.onclick = function () {
+        const listItem = this.parentElement;
+        removeElementFromParent(listItem);
+        saveListToLocalStorage();
+    }
+
+    const closeButtonIcon = document.createTextNode('\u00D7');
+    closeButton.appendChild(closeButtonIcon);
+
+    return closeButton;
 }
 
-// Creating a new list item when clicking on 'Add' button
-function addItem() {
-    const inputValue = document.getElementById('myInput').value;
-    if (inputValue === '') {
+function removeElementFromParent(child) {
+    child.parentElement.removeChild(child);
+}
+
+function addNewListItem() {
+    const newListItemValue = document.getElementById('myInput').value;
+    if (!newListItemValue) {
         alert('What would you like ToDo?');
         return;
     }
 
-    const item = {
-        value: inputValue,
+    addListItem({
+        value: newListItemValue,
         checked: false
-    };
-
-    addItemToList(item);
+    });
 
     saveListToLocalStorage();
 
     document.getElementById('myInput').value = '';
 }
 
-function clearAll() {
+function clearAllListItems() {
     while (list.hasChildNodes()) {
         list.removeChild(list.firstChild)
     }
+
     saveListToLocalStorage();
 }
 
 function saveListToLocalStorage() {
     let itemsArray = [];
+
     for (let i = 0; i < listItems.length; i++) {
-        const item = {
+        itemsArray.push({
             value: listItems[i].firstChild.textContent,
             checked: listItems[i].classList.contains('checked')
-        };
-
-        itemsArray.push(item);
+        });
     }
 
     localStorage.setItem('items', JSON.stringify(itemsArray));
@@ -72,15 +76,13 @@ function loadListFromLocalStorage() {
     if (localStorage.getItem('items')) {
         const itemsArray = JSON.parse(localStorage.getItem('items'));
         for (let i = 0; i < itemsArray.length; i++) {
-            addItemToList(itemsArray[i]);
+            addListItem(itemsArray[i]);
         }
     }
 }
 
-loadListFromLocalStorage();
-
-function addItemToList(item) {
-    const listItem = document.createElement('li');
+function addListItem(item) {
+    const listItem = document.createElement('LI');
     const listItemText = document.createTextNode(item.value);
     listItem.appendChild(listItemText);
     initListItem(listItem);
@@ -92,3 +94,4 @@ function addItemToList(item) {
     list.appendChild(listItem);
 }
 
+loadListFromLocalStorage();
